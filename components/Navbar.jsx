ec81ee20272d116e0642cb39a5bbc93de4a7d93a
MisterCruz <pspnando@me.com>
2023-01-23 12:29:16 -0800

    add routing to home

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black'>
         <Link href="/"><h1 className='w-full text-3xl text-transparent font-bold bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>GlowAI</h1></Link>
         <ul className="hidden md:flex">
-            <li className='p-4 font-bold cursor-pointer'>Home</li>
+            <Link href="/"><li className='p-4 font-bold cursor-pointer'>Home</li></Link>
             <li className='p-4 font-bold cursor-pointer'>About</li>
             <li className='p-4 font-bold cursor-pointer'>Contact</li>
         </ul>       
