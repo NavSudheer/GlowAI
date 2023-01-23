099f955e1d558dcd3c4ff343a7eccb67e7f60cd9
MisterCruz <pspnando@me.com>
2023-01-23 13:47:44 -0800

    add AI response

--- pages/Routine.js
+++ pages/Routine.js
@@ -5,6 +5,30 @@ const Routine = () => {
   const [budgetInput, setBudgetInput] = useState("");
   const [skinTypeInput, setSkinTypeInput] = useState("");
 
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log(`Calling OpenAI...`);
+    const res = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ budgetInput, skinTypeInput }),
+    });
+    const data = await res.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text);
+    console.log(budgetInput);
+    console.log(skinTypeInput);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
+
   const onBudgetChangedText = (e) => {
     console.log(e.target.value);
     setBudgetInput(e.target.value);
@@ -50,11 +74,23 @@ const Routine = () => {
           onChange={onSkinTypeChangedText}
         />
         <button
-          onClick={null}
+          onClick={callGenerateEndpoint}
           className="bg-stone-500 hover:bg-stone-600 text-white font-bold py-2 px-4 mt-10 rounded justify-center items-center"
         >
           Generate routine
         </button>
+        {apiOutput && (
+          <div className="flex flex-col justify-center items-center mt-10">
+            <div>
+              <div>
+                <h3 className="text-blue-800">Output</h3>
+              </div>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,3 +5,24 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix =
+  "Create a custom skincare routine. Based on my budget and skin type: ";
+const generateAction = async (req, res) => {
+  //run first prompt
+  console.log(
+    `API: ${basePromptPrefix}${req.body.budgetInput}${req.body.skinTypeInput}`
+  );
+
+  const baseCompletion = await openai.createCompletion({
+    model: `text-davinci-003`,
+    prompt: `${basePromptPrefix}${req.body.budgetInput} ${req.body.skinTypeInput}`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+
+  const basePromptOutput = baseCompletion.data.choices.pop();
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
