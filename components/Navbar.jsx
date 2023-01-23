d0b9d682217a6dee29a4f7154ab51ee5fca636b1
MisterCruz <pspnando@me.com>
2023-01-23 12:24:58 -0800

    add routing to homepage

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react'
-// import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import Link from 'next/link'
 
 const Navbar = () => {
    
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black'>
-        <h1 className='w-full text-3xl text-transparent font-bold bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>GlowAI</h1>
+        <Link href="/"><h1 className='w-full text-3xl text-transparent font-bold bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>GlowAI</h1></Link>
         <ul className="hidden md:flex">
             <li className='p-4 font-bold cursor-pointer'>Home</li>
             <li className='p-4 font-bold cursor-pointer'>About</li>
